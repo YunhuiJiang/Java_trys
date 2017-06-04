a081af98a0926cb8e7ea16bdb1790e4a8b8316a8
JiangYunhui <JiangYunhui@J>
2017-06-04 18:49:16 +0800

    Signed-off-by: JiangYunhui <JiangYunhui@J>

--- src/hanoiProblem1/HanoiProblem1.java
+++ src/hanoiProblem1/HanoiProblem1.java
@@ -2,7 +2,7 @@ package hanoiProblem1;
 
 import java.util.Stack;
 
-//hanoi濉斾慨鏀圭増锛岄檺鍒舵父鎴忚鍒欎负涓嶈兘浠庡乏鐩存帴鍒板彸
+//hanoi塔修改版，限制游戏规则为不能从左直接到右
 public class HanoiProblem1 {
 	public static void main(String[] args) {
 		String left="left";
